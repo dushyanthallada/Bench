9b18047aee2ec492ca491dfc1f207d16c2a6a48a
Allada, Dushyanth Naidu <92858953+dushyanthallada@users.noreply.github.com>
2023-01-25 13:34:23 +0530

    first commit

---
+++ ZBench/src/beginner1/firstcode/First.java
@@ -0,0 +1,10 @@
+package beginner1.firstcode;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the First");
+	}
+
+}
